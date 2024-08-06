5973f0989a4a25792971f004b2f3f5da6593753d
oas!s <155007644+0asisCat@users.noreply.github.com>
2024-08-06 11:12:00 +0800

    Initializing special keys

---
+++ script.js
@@ -0,0 +1,10 @@
+// Special Keys
+const backSpace = document.querySelector("#kr-backspace");
+const capsLock = document.querySelector("#kr-caps-lock");
+const space = document.querySelector("#kr-space");
+const enter = document.querySelector("#kr-enter");
+const shift = document.querySelector("#kr-shift");
+const ctrl = document.querySelector("#kr-ctrl");
+const alt = document.querySelector("#kr-alt");
+const tab = document.querySelector("kr-tab");
+// Keys
